ae8c77b60b608adfb74c98f6cb23b07ab8851f51
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-11-21 17:51:21 +0530

    Added degreeToRadian.js

---
+++ degreeToRadian.js
@@ -0,0 +1,10 @@
+const degreeToRadian = (deg) => (deg * Math.PI) / 180.0;
+
+let temp1 = 360;
+console.log(degreeToRadian(temp1));
+
+let temp2 = 180;
+console.log(degreeToRadian(temp2));
+
+let temp3 = 120;
+console.log(degreeToRadian(temp3));
