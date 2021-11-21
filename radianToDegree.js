eb48ba5876df5cc4725ae84892ff8f85ae39e7aa
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-11-21 17:55:13 +0530

    Added radianToDegree.js

---
+++ radianToDegree.js
@@ -0,0 +1,10 @@
+const radianToDegree = (rad) => (rad * 180) / Math.PI;
+
+let temp1 = 6.283185307179586;
+console.log(radianToDegree(temp1));
+
+let temp2 = 3.141592653589793;
+console.log(radianToDegree(temp2));
+
+let temp3 = 2.0943951023931953;
+console.log(radianToDegree(temp3));
